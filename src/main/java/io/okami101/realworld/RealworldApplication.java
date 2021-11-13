96a315eb73f84ee76efd03be765ebd993d54483b
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-11-13 16:12:44 +0100

    init

---
+++ src/main/java/io/okami101/realworld/RealworldApplication.java
@@ -0,0 +1,13 @@
+package io.okami101.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldApplication.class, args);
+	}
+
+}
