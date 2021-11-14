fc83c87026972ac7bd76065bd0b70c52c22f7911
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-11-15 00:49:03 +0100

    cleanup

--- src/main/java/io/okami101/realworld/api/UserApi.java
+++ src/main/java/io/okami101/realworld/api/UserApi.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.okami101.realworld.application.user.UpdateUserRequest;
