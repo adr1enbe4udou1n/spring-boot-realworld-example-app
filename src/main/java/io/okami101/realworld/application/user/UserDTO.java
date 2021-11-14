f6211375548b044640b25bcfdf5a61b5b472675c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-11-14 21:55:06 +0100

    refactor

---
+++ src/main/java/io/okami101/realworld/application/user/UserDTO.java
@@ -0,0 +1,21 @@
+package io.okami101.realworld.application.user;
+
+import io.okami101.realworld.core.user.User;
+import lombok.Getter;
+
+@Getter
+public class UserDTO {
+    private String email;
+    private String username;
+    private String bio;
+    private String image;
+    private String token;
+
+    public UserDTO(User user, String token) {
+        this.email = user.getEmail();
+        this.username = user.getName();
+        this.bio = user.getBio();
+        this.image = user.getImage();
+        this.token = token;
+    }
+}
---
+++ src/main/java/io/okami101/realworld/application/user/UserResponse.java
@@ -0,0 +1,12 @@
+package io.okami101.realworld.application.user;
+
+import lombok.Getter;
+
+@Getter
+public class UserResponse {
+    private UserDTO user;
+
+    public UserResponse(UserDTO user) {
+        this.user = user;
+    }
+}
