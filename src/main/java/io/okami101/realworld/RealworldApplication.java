efcfe302e2cda7f89b15f26b9a781897683d8701
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-11-20 18:04:41 +0100

    dev faker

---
+++ src/main/java/io/okami101/realworld/RealworldApplication.java
@@ -0,0 +1,20 @@
+package io.okami101.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class RealworldApplication {
+
+	public static void main(String[] args) {
+		if (args.length == 1 && args[0].equals("--seed")) {
+			new SpringApplicationBuilder(RealworldApplication.class).web(WebApplicationType.NONE).run(args);
+			return;
+		}
+
+		SpringApplication.run(RealworldApplication.class, args);
+	}
+
+}
--- src/main/java/io/okami101/realworld/cli/SeederCommand.java
+++ src/main/java/io/okami101/realworld/cli/SeederCommand.java
@@ -1,47 +1,120 @@
 package io.okami101.realworld.cli;
 
-import java.util.concurrent.Callable;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import com.github.javafaker.Faker;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import io.okami101.realworld.core.article.Article;
+import io.okami101.realworld.core.article.ArticleRepository;
+import io.okami101.realworld.core.article.Comment;
+import io.okami101.realworld.core.article.CommentRepository;
+import io.okami101.realworld.core.article.Tag;
+import io.okami101.realworld.core.article.TagRepository;
+import io.okami101.realworld.core.service.SlugService;
 import io.okami101.realworld.core.user.PasswordHashService;
 import io.okami101.realworld.core.user.User;
-import picocli.CommandLine.Command;
+import io.okami101.realworld.core.user.UserRepository;
 
-@SpringBootApplication
-@Command(name = "seederCommand")
-public class SeederCommand implements Callable<Integer> {
+@Component
+public class SeederCommand implements CommandLineRunner {
 
     @Autowired
-    protected EntityManager em;
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected ArticleRepository articleRepository;
+
+    @Autowired
+    protected TagRepository tagRepository;
+
+    @Autowired
+    protected CommentRepository commentRepository;
 
     @Autowired
     protected PasswordHashService passwordHashService;
+
+    @Autowired
+    protected SlugService slugService;
 
     @Autowired
     protected Flyway flyway;
 
     @Override
     @Transactional
-    public Integer call() throws Exception {
+    public void run(String... args) {
+        if (args.length == 0 || !args[0].equals("--seed")) {
+            return;
+        }
+
         flyway.clean();
         flyway.migrate();
 
-        User user = new User();
-        user.setName("John Doe");
-        user.setEmail("john.doe@example.com");
-        user.setBio("John Bio");
-        user.setImage("https://randomuser.me/api/portraits/men/1.jpg");
+        Faker faker = new Faker();
 
-        em.persist(user);
-        em.flush();
+        String password = passwordHashService.hash("password");
 
-        return 0;
+        for (int i = 0; i < 50; i++) {
+            User user = new User();
+            user.setName(faker.name().fullName());
+            user.setPassword(password);
+            user.setEmail(faker.internet().emailAddress());
+            user.setBio(faker.lorem().paragraphs(3).stream().collect(Collectors.joining("\n")));
+            user.setImage("https://randomuser.me/api/portraits/men/" + faker.number().numberBetween(1, 99) + ".jpg");
+
+            userRepository.save(user);
+        }
+
+        List<User> users = userRepository.findAll();
+
+        users.forEach(user -> {
+            for (int i = 0; i < faker.number().numberBetween(1, 3); i++) {
+                user.getFollowers().add(users.get(faker.number().numberBetween(0, users.size() - 1)));
+                userRepository.save(user);
+            }
+        });
+
+        for (int i = 0; i < 30; i++) {
+            tagRepository.save(new Tag(faker.lorem().words(2).stream().collect(Collectors.joining(" "))));
+        }
+
+        List<Tag> tags = tagRepository.findAll();
+
+        for (int i = 0; i < 500; i++) {
+            String title = faker.lorem().sentence();
+
+            Article article = new Article();
+            article.setTitle(title.substring(0, 1).toUpperCase() + title.substring(1));
+            article.setSlug(slugService.generate(title));
+            article.setDescription(faker.lorem().paragraph());
+            article.setBody(faker.lorem().paragraphs(5).stream().collect(Collectors.joining("\n")));
+            article.setAuthor(users.get(faker.number().numberBetween(0, 49)));
+
+            for (int j = 0; j < faker.number().numberBetween(1, 3); j++) {
+                article.getTags().add(tags.get(faker.number().numberBetween(0, tags.size() - 1)));
+            }
+
+            for (int j = 0; j < faker.number().numberBetween(1, 10); j++) {
+                article.getFavoritedBy().add(users.get(faker.number().numberBetween(0, users.size() - 1)));
+            }
+
+            articleRepository.save(article);
+
+            for (int j = 0; j < faker.number().numberBetween(5, 10); j++) {
+                Comment comment = new Comment();
+                comment.setBody(faker.lorem().paragraph());
+                comment.setAuthor(users.get(faker.number().numberBetween(0, users.size() - 1)));
+                comment.setArticle(article);
+                commentRepository.save(comment);
+            }
+        }
     }
 
 }
