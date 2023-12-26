a2c050a3e2f33019e146a46562ff78f79686bed9
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-12-26 05:49:25 +0100

    n+1

--- src/main/java/io/okami101/realworld/core/article/ArticleRepository.java
+++ src/main/java/io/okami101/realworld/core/article/ArticleRepository.java
@@ -4,6 +4,7 @@ import io.okami101.realworld.core.user.User;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
@@ -11,14 +12,19 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
   Optional<Article> findBySlug(String slug);
 
+  @EntityGraph(attributePaths = {"author.followers", "tags", "favoritedBy"})
   Page<Article> findAllByOrderByIdDesc(Pageable pageable);
 
+  @EntityGraph(attributePaths = {"author.followers", "tags", "favoritedBy"})
   Page<Article> findAllByFavoritedByOrderByIdDesc(User user, Pageable pageable);
 
+  @EntityGraph(attributePaths = {"author.followers", "tags", "favoritedBy"})
   Page<Article> findAllByAuthorNameOrderByIdDesc(String author, Pageable pageable);
 
+  @EntityGraph(attributePaths = {"author.followers", "tags", "favoritedBy"})
   Page<Article> findAllByTagsOrderByIdDesc(Tag tag, Pageable pageable);
 
+  @EntityGraph(attributePaths = {"author.followers", "tags", "favoritedBy"})
   Page<Article> findAllByAuthorFollowersOrderByIdDesc(User user, Pageable pageable);
 
   boolean existsBySlug(String value);
