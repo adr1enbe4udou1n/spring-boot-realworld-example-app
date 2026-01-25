390a2e532c044a1b45830168c4b511440810d708
Adrien Beaudouin <3679080+adr1enbe4udou1n@users.noreply.github.com>
2026-01-25 16:13:24 +0100

    fix hibernate syntax

--- src/main/java/io/okami101/realworld/core/article/ArticleRepository.java
+++ src/main/java/io/okami101/realworld/core/article/ArticleRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
   Optional<Article> findByTitle(String title);
@@ -17,13 +19,15 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
   Page<Article> findAllByOrderByIdDesc(Pageable pageable);
 
   @EntityGraph(attributePaths = {"author"})
-  Page<Article> findAllByFavoritedByOrderByIdDesc(User user, Pageable pageable);
+  @Query("SELECT a FROM Article a WHERE :user MEMBER OF a.favoritedBy ORDER BY a.id DESC")
+  Page<Article> findAllByFavoritedByOrderByIdDesc(@Param("user") User user, Pageable pageable);
 
   @EntityGraph(attributePaths = {"author"})
   Page<Article> findAllByAuthorNameOrderByIdDesc(String author, Pageable pageable);
 
   @EntityGraph(attributePaths = {"author"})
-  Page<Article> findAllByTagsOrderByIdDesc(Tag tag, Pageable pageable);
+  @Query("SELECT a FROM Article a WHERE :tag MEMBER OF a.tags ORDER BY a.id DESC")
+  Page<Article> findAllByTagsOrderByIdDesc(@Param("tag") Tag tag, Pageable pageable);
 
   @EntityGraph(attributePaths = {"author"})
   Page<Article> findAllByAuthorFollowersOrderByIdDesc(User user, Pageable pageable);
